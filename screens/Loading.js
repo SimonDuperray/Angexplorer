5be44cf47296ad1c16ed3179815d7845b480db12
Kartodix <64204457+Kartodix@users.noreply.github.com>
2021-02-25 16:35:57 +0100

    struct & Loading v1

--- screens/Loading.js
+++ screens/Loading.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Svg from 'react-native-svg';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import logo from './assets/logoreduitnoir.svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text style={{color: 'white'}}>Open up App.js to start working on your app !</Text> */}
       <Image source={logo} style={{width: 200, height: 300, aspectRatio: 2/3}} />
-      {/* <Svg height="100%" width="100%" viewBox="0 0 100 100">
-        <Image source={logo} style={{height: undefined, width: '100%', aspectRatio: 1}} />
-      </Svg> */}
     </View>
   );
 }
